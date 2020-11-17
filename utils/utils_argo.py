b3584c9dc0daf06880524e7d66d297cbef84e15f
jc3342 <45376818+jc3342@users.noreply.github.com>
2020-11-17 15:57:53 -0500

    Update utils_argo.py

--- utils/utils_argo.py
+++ utils/utils_argo.py
@@ -1,8 +1,10 @@
-
+import torch
 from typing import Any, Dict, List, Tuple
 import pandas as pd
 import numpy as np
 import argparse
+import math
+from numpy import cos, sin
 
 
 RAW_DATA_FORMAT = {
@@ -23,14 +25,17 @@ STATIONARY_THRESHOLD = (
 
 NEARBY_DISTANCE_THRESHOLD = 30
 
+def transform(theta, tracks_obs):
+    th = -theta + np.pi/2
+    Rot = np.array([[cos(th),-sin(th)],[sin(th),cos(th)]])
+    new_pos = (np.dot(Rot, tracks_obs.reshape(-1,2).T)).T
+    return new_pos
 
 
 def compute_velocity(track_df: pd.DataFrame) -> List[float]:
     """Compute velocities for the given track.
-
     Returns:
         vel (list of float): Velocity at each timestep
-
     """
     x_coord = track_df["X"].values
     y_coord = track_df["Y"].values
@@ -49,12 +54,10 @@ def compute_velocity(track_df: pd.DataFrame) -> List[float]:
 
 def get_is_track_stationary(track_df: pd.DataFrame) -> bool:
     """Check if the track is stationary.
-
     Args:
         track_df (pandas Dataframe): Data for the track
     Return:
         _ (bool): True if track is stationary, else False 
-
     """
     vel = compute_velocity(track_df)
     sorted_vel = sorted(vel)
@@ -70,7 +73,6 @@ def pad_track(
         raw_data_format: Dict[str, int],
 ) -> np.ndarray:
     """Pad incomplete tracks.
-
     Args:
         track_df (Dataframe): Dataframe for the track
         seq_timestamps (numpy array): All timestamps in the sequence
@@ -78,7 +80,6 @@ def pad_track(
         raw_data_format (Dict): Format of the sequence
     Returns:
             padded_track_array (numpy array): Track data padded in front and back
-
     """
     track_vals = track_df.values
     track_timestamps = track_df["TIMESTAMP"].values
@@ -122,14 +123,12 @@ def fill_track_lost_in_middle(
         raw_data_format: Dict[str, int],
 ) -> np.ndarray:
     """Handle the case where the object exited and then entered the frame but still retains the same track id. It'll be a rare case.
-
     Args:
         track_array (numpy array): Padded data for the track
         seq_timestamps (numpy array): All timestamps in the sequence
         raw_data_format (Dict): Format of the sequence
     Returns:
         filled_track (numpy array): Track data filled with missing timestamps
-
     """
     curr_idx = 0
     filled_track = np.empty((0, track_array.shape[1]))
@@ -144,14 +143,12 @@ def fill_track_lost_in_middle(
 def filter_tracks(seq_df: pd.DataFrame, obs_len: int, agent_track_obs: np.ndarray,
                   raw_data_format: Dict[str, int] = RAW_DATA_FORMAT) -> np.ndarray :
     """Pad tracks which don't last throughout the sequence. Also, filter out non-relevant tracks.
-
     Args:
         seq_df (pandas Dataframe): Dataframe containing all the tracks in the sequence
         obs_len (int): Length of observed trajectory
         raw_data_format (Dict): Format of the sequence
     Returns:
         social_tracks (numpy array): Array of relevant tracks
-
     """
     social_tracks = np.empty((0, obs_len, len(raw_data_format)))
 
@@ -233,6 +230,100 @@ def compute(seq_path: str, obs_len) -> np.ndarray:
     tracks_obs = np.concatenate((np.expand_dims(agent_track_obs,axis=0), social_tracks_obs),axis = 0)
     return tracks_obs, agent_track_pred
 
+
+def process_data(data_path, obs_len, pred_len):
+    # argo_data = argo_create(data_path)
+
+    # count = 0
+    # for scene_name in argo_data.keys():
+        
+    # read data
+    tracks_obs, agent_track_pred = compute(data_path, obs_len)
+
+    # create edge_index
+    # num_obj = tracks_obs.shape[0]
+    # edge_index_row2 = torch.linspace(1, num_obj-1, steps=num_obj-1).view(1,-1)
+    # edge_index_row1 = torch.zeros(num_obj - 1).view(1,-1)
+    # edge_index = torch.cat((edge_index_row1, edge_index_row2),0)
+    # edge_index = edge_index.long()
+
+    # 
+    tracks_obs = tracks_obs[0,:,3:5].astype('float64')
+    tracks_obs = np.expand_dims(tracks_obs, axis=0) 
+    agent_track_pred = agent_track_pred[:,3:5].astype('float64')
+
+
+    # normalize            
+    agent_track_pred = agent_track_pred - tracks_obs[0,-1,:]
+    tracks_obs = tracks_obs - tracks_obs[0,-1,:]  
+
+    # cal velocity
+    v = np.zeros_like(tracks_obs)
+    for i in range(tracks_obs.shape[0]):
+        for j in range(tracks_obs.shape[1]-1):
+            v[i,j,:] = tracks_obs[i,j+1,:] - tracks_obs[i,j,:]
+        v[i,-1,:] = v[i,-2,:]
+    
+    # cal acceleration
+    a = np.zeros_like(tracks_obs)
+    for i in range(v.shape[0]):
+        for j in range(a.shape[1]-1):
+            a[i,j,:] = v[i,j+1,:] - v[i,j,:]
+        a[i,-1,:] = a[i,-2,:]
+
+    # cal theta
+    theta = np.zeros(v.shape[1])
+    for k in range(v.shape[1]):
+        if v[0,k,1]>0 and v[0,k,0] >0:
+            theta[k] = math.atan(v[0,k,1]/v[0,k,0])
+        if v[0,k,1]>0 and v[0,k,0] <0:
+            theta[k] = np.pi - math.atan(v[0,k,1]/(-v[0,k,0]))
+        if v[0,k,1]<0 and v[0,k,0] <0:
+            theta[k] = math.atan((-v[0,k,1])/(-v[0,k,0])) + np.pi
+        if v[0,k,1]<0 and v[0,k,0] >0:
+            theta[k] = -math.atan((-v[0,k,1])/v[0,k,0])
+
+        if theta[k]>2*np.pi:
+            theta[k,0] = theta[k] - 2*np.pi
+        if theta[k]<0:
+            theta[k] = theta[k] + 2*np.pi
+
+        if k>0:
+            if theta[k]-theta[k-1]>np.pi:
+                theta[k] -= 2*np.pi
+            elif theta[k]-theta[k-1]<-np.pi:
+                theta[k] += 2*np.pi        
+    
+
+    # transform coodinate
+    # obs
+    th = np.mean(theta[-4:])
+    track_obs = transform(th, tracks_obs).reshape(-1,obs_len,2)
+    v = transform(th, v).reshape(-1,obs_len,2)
+    a = transform(th, a).reshape(-1,obs_len,2)
+    new_tracks_obs = np.concatenate((track_obs,v,a),2)
+    # pred
+    new_agent_pred = transform(th, agent_track_pred).reshape(pred_len,2)
+
+
+
+    x = new_tracks_obs
+    y = new_agent_pred
+
+    return x, y 
+
+
+def window_shift(inputs, label):
+    # inputs: 20x6 [x, y, vx, vy, ax, ay]
+    # label: 1x2 [x, y]
+    vx, vy = label[0] - inputs[0,-1,0],  label[1] - inputs[0,-1,1]
+    ax, ay = vx - inputs[0,-1,2], vy - inputs[0,-1,3]
+
+    label = torch.tensor([[[label[0], label[1], vx, vy, ax, ay]]])
+    inputs = torch.cat((inputs,label),1)
+
+    #inputs = torch.from_numpy(inputs[1:,:]).float()
+    return inputs[:,1:,:]
 
 
 
